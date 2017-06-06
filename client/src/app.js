276fce3575f5fd1335b4f8bce38f0ecdfbdf8b58
t-kusakabe <ttmac0704@gmail.com>
2017-06-06 18:44:36 +0900

    modify react-router

--- client/src/app.js
+++ client/src/app.js
@@ -1,20 +1,32 @@
 import React, { Component } from 'react';
 import { render } from 'react-dom';
-import Articles from './articles/articles';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 
+import Articles from './articles/articles';
+import Form from './articles/form';
 
 class App extends Component {
   render() {
     return (
       <div>
-        <Articles />
+        <ul>
+          <li><Link to='/'>root</Link></li>
+          <li><Link to='/articles'>article</Link></li>
+          <li><Link to='/form'>test link</Link></li>
+        </ul>
       </div>
     );
   }
-};
+}
 
-render (
-  <App />,
-  document.getElementById('app')
-)
+render ((
+  <Router>
+    <div>
+      <Route path='/' component={App} />
+      <Route path='/articles/' component={Articles} />
+      <Route path='/form' component={Form} />
+    </div>
+  </Router>
+  ), document.getElementById('app')
+);
 
