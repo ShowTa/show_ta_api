e55c7ecc4052a5ccef0f8f0d626c692f38eac3d5
t-kusakabe <ttmac0704@gmail.com>
2017-06-15 18:43:52 +0900

    crteate article/detail component

--- client/src/articles/detail.js
+++ client/src/articles/detail.js
@@ -1,18 +1,34 @@
 import React, { Component } from 'react';
 import { render  } from 'react-dom';
-
-const REQUEST_URL = 'http://localhost:3000/articles/show';
+import request from 'superagent';
 
 export default class Detail extends Component {
   constructor(props) {
     super(props);
-    this.state = {status: true};
+    this.state = {
+      article: []
+    };
+  }
+
+  componentDidMount() {
+    this.fetchDeta();
+  }
+
+  fetchDeta() {
+    request
+      .get('http://localhost:3000' + this.props.match.url)
+      .end((err, res) => {
+        this.setState({
+          article: res.body
+        });
+      });
   }
 
   render() {
     return (
       <div>
-        show
+        <h1>{this.state.article.title}</h1>
+        <article>{this.state.article.content}</article>
       </div>
     );
   }
